8bd19155fbd11b792f7539dab2d78c823b2047c7
J-K. Solbakken <jksolbakken@gmail.com>
2024-08-16 15:55:04 +0200

    really simple webapp

--- cmd/kumodemo/main.go
+++ cmd/kumodemo/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "KU-MO says hello @ %s", time.Now().Format(time.RFC822))
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+	fmt.Println("Starting...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
