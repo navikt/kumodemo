81ad584bc29d56ae5e195486a4ea9ec29c3bbff8
J-K. Solbakken <jksolbakken@gmail.com>
2024-08-17 20:12:34 +0200

    naisify

--- cmd/kumodemo/main.go
+++ cmd/kumodemo/main.go
@@ -8,12 +8,21 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/isAlive", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+
+	http.HandleFunc("/isReady", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		_, err := fmt.Fprintf(w, "KU-MO says hello @ %s", time.Now().Format(time.RFC1123))
 		if err != nil {
 			fmt.Println(err)
 		}
 	})
+
 	fmt.Println("Starting...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
