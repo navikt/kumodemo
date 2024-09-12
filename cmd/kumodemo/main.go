d6a5593186edf21a4a66bae37362082ddfdf17c6
J-K. Solbakken <jksolbakken@gmail.com>
2024-09-12 13:21:11 +0200

    demo

--- cmd/kumodemo/main.go
+++ cmd/kumodemo/main.go
@@ -18,7 +18,7 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("content-type", "text/html")
-		_, err := fmt.Fprintf(w, "<h2>KU-MO demo says hello @ %s</h2>", time.Now().Format(time.RFC1123))
+		_, err := fmt.Fprintf(w, "<h2>KU-MO demo says hi @ %s</h2>", time.Now().Format(time.RFC1123))
 		if err != nil {
 			fmt.Println(err)
 		}
